d259a6e511110887ac7248bdfc6975fe241a1e6e
Mark Eaton <MarkEEaton@users.noreply.github.com>
2019-12-08 15:53:57 -0500

    simplify for only one google api key

--- make-spreadsheet.py
+++ make-spreadsheet.py
@@ -21,8 +21,8 @@ def main():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token-gmail.pickle"):
-        with open("token-gmail.pickle", "rb") as token:
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -30,11 +30,11 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials-gmail.json", SCOPES
+                "credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token-gmail.pickle", "wb") as token:
+        with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
 
     service = build("gmail", "v1", credentials=creds)
